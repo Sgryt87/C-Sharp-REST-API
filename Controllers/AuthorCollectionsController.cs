a6d7a4cbbee44a81b6db56658a0709cc659aa639
SergiiG <sergii.grytsaienko@accenture.com>
2019-02-09 19:40:24 -0600

    created a collection of resourses

---
+++ Controllers/AuthorCollectionsController.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using Library.API.Entities;
+using Library.API.Models;
+using Library.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.API.Controllers
+{
+    [Route("api/authorcollections")]
+    public class AuthorCollectionsController : Controller
+    {
+        private ILibraryRepository _libraryRepository;
+        public AuthorCollectionsController(ILibraryRepository libraryRepository)
+        {
+            _libraryRepository = libraryRepository;
+        }
+
+        [HttpPost]
+        public IActionResult CreateAuthorCollection([FromBody] IEnumerable<AuthorForCreationDto> authorColletion)
+        {
+            if(authorColletion == null)
+            {
+                return BadRequest();
+            }
+
+            var authorEntities = Mapper.Map<IEnumerable<Author>>(authorColletion);
+
+            foreach (var author in authorEntities)
+            {
+                _libraryRepository.AddAuthor(author);
+            }
+            if (!_libraryRepository.Save())
+            {
+                throw new Exception("Creating an author failed on save");
+            }
+            return Ok();
+        }
+    }
+}
