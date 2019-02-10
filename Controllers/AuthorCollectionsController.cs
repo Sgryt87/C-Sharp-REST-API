5d8a6e4c339c23a4fd124d8cd1b2ae5f0491657c
SergiiG <sergii.grytsaienko@accenture.com>
2019-02-09 20:08:24 -0600

    working with array keys and composite keys (model binding)

--- Controllers/AuthorCollectionsController.cs
+++ Controllers/AuthorCollectionsController.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Library.API.Entities;
+using Library.API.Helpers;
 using Library.API.Models;
 using Library.API.Services;
 using Microsoft.AspNetCore.Mvc;
@@ -14,30 +15,63 @@ namespace Library.API.Controllers
     public class AuthorCollectionsController : Controller
     {
         private ILibraryRepository _libraryRepository;
+
         public AuthorCollectionsController(ILibraryRepository libraryRepository)
         {
             _libraryRepository = libraryRepository;
         }
 
         [HttpPost]
-        public IActionResult CreateAuthorCollection([FromBody] IEnumerable<AuthorForCreationDto> authorColletion)
+        public IActionResult CreateAuthorCollection(
+            [FromBody] IEnumerable<AuthorForCreationDto> authorCollection)
         {
-            if(authorColletion == null)
+            if (authorCollection == null)
             {
                 return BadRequest();
             }
 
-            var authorEntities = Mapper.Map<IEnumerable<Author>>(authorColletion);
+            var authorEntities = Mapper.Map<IEnumerable<Author>>(authorCollection);
 
             foreach (var author in authorEntities)
             {
                 _libraryRepository.AddAuthor(author);
             }
+
             if (!_libraryRepository.Save())
             {
-                throw new Exception("Creating an author failed on save");
+                throw new Exception("Creating an author collection failed on save.");
             }
-            return Ok();
+
+            var authorCollectionToReturn = Mapper.Map<IEnumerable<AuthorDto>>(authorEntities);
+            var idsAsString = string.Join(",",
+                authorCollectionToReturn.Select(a => a.Id));
+
+            return CreatedAtRoute("GetAuthorCollection",
+                new { ids = idsAsString },
+                authorCollectionToReturn);
+            //return Ok();
+        }
+
+        // (key1,key2, ...)
+
+        [HttpGet("({ids})", Name = "GetAuthorCollection")]
+        public IActionResult GetAuthorCollection(
+            [ModelBinder(BinderType = typeof(ArrayModelBinder))] IEnumerable<Guid> ids)
+        {
+            if (ids == null)
+            {
+                return BadRequest();
+            }
+
+            var authorEntities = _libraryRepository.GetAuthors(ids);
+
+            if (ids.Count() != authorEntities.Count())
+            {
+                return NotFound();
+            }
+
+            var authorsToReturn = Mapper.Map<IEnumerable<AuthorDto>>(authorEntities);
+            return Ok(authorsToReturn);
         }
     }
 }
